a8301b6feb8bd3d59bff0e367e5a6a0029b7a478
me3za <medkrix1@gmail.com>
2022-11-27 06:21:45 +0100

    added progress updating api call to backend and split tasks container into multiple smaller modules

--- dtb-backend/controllers/task.js
+++ dtb-backend/controllers/task.js
@@ -7,61 +7,86 @@ const config = require('../utils/config.js');
 
 //add a task
 taskRouter.post('/addTask/:id', async (req, res) => {
-  const token = req.token;
-  try {
-    const decodedToken = jwt.verify(token, config.SECRET);
+	try {
+		const token = req.token;
+		jwt.verify(token, config.SECRET);
 
-    const week = await Week.findById(req.params.id);
+		const week = await Week.findById(req.params.id);
 
-    const task = new Task({
-      title: req.body.title,
-      description: req.body.description,
-      priority: req.body.priority,
-      progress: req.body.progress,
-      week: week._id,
-    });
-    const savedTask = await task.save();
+		const task = new Task({
+			title: req.body.title,
+			description: req.body.description,
+			priority: req.body.priority,
+			progress: req.body.progress,
+			week: week._id,
+		});
+		const savedTask = await task.save();
 
-    week.tasks = week.tasks.concat(savedTask._id);
-    await week.save();
-    return res.status(200).json({
-      title: savedTask.title,
-      description: savedTask.description,
-      priority: savedTask.priority,
-      progress: savedTask.progress,
-      id: savedTask._id,
-    });
-  } catch (err) {
-    console.log('task router', err);
-    return res.status(500).json({ erro: err });
-  }
+		week.tasks = week.tasks.concat(savedTask._id);
+		await week.save();
+		return res.status(200).json({
+			title: savedTask.title,
+			description: savedTask.description,
+			priority: savedTask.priority,
+			progress: savedTask.progress,
+			id: savedTask._id,
+		});
+	} catch (err) {
+		console.log('task router', err);
+		return res.status(500).json({ erro: err });
+	}
 });
 
 //edit task
 taskRouter.put('/editTask', async (req, res) => {
-  const token = req.token;
-  try {
-    const decodedToken = jwt.verify(token, config.SECRET);
+	try {
+		const token = req.token;
+		jwt.verify(token, config.SECRET);
 
-    const task = await Task.findById(req.body.id);
+		const task = await Task.findById(req.body.id);
 
-    task.title = req.body.title;
-    task.description = req.body.description;
-    task.priority = req.body.priority;
+		task.title = req.body.title;
+		task.description = req.body.description;
+		task.priority = req.body.priority;
 
-    const savedTask = await task.save();
+		const savedTask = await task.save();
 
-    return res.status(200).json({
-      title: savedTask.title,
-      description: savedTask.description,
-      priority: savedTask.priority,
-      progress: savedTask.progress,
-      id: savedTask._id,
-    });
-  } catch (err) {
-    console.log('task router', err);
-    return res.status(500).json({ erro: err });
-  }
+		return res.status(200).json({
+			title: savedTask.title,
+			description: savedTask.description,
+			priority: savedTask.priority,
+			progress: savedTask.progress,
+			id: savedTask._id,
+		});
+	} catch (err) {
+		console.log('task router', err);
+		return res.status(500).json({ erro: err });
+	}
+});
+
+//edit task
+taskRouter.put('/changeProgress', async (req, res) => {
+	try {
+		const token = req.token;
+		jwt.verify(token, config.SECRET);
+
+		const task = await Task.findById(req.body.id);
+
+		task.progress = req.progress;
+
+		const savedTask = await task.save();
+
+		return res.status(200).json({
+			title: savedTask.title,
+			description: savedTask.description,
+			priority: savedTask.priority,
+			progress: savedTask.progress,
+			id: savedTask._id,
+		});
+	} catch (err) {
+		console.log('task router', err);
+		return res.status(500).json({ erro: err });
+	}
 });
 
 module.exports = taskRouter;
---
+++ dtb-frontend/src/components/EditTaskForm/EditTaskForm.js
@@ -0,0 +1,158 @@
+import { useEffect, useRef, useState } from 'react';
+import TaskServices from '../../services/task.js';
+
+const EditTaskForm = ({
+	user,
+	taskToEdit,
+	setTaskToEdit,
+	tasks,
+	setTasks,
+	setFormToOpen,
+}) => {
+	const initialRender = useRef(true);
+	const [nameField, setNameField] = useState(taskToEdit.title);
+	const [descriptionField, setDescriptionField] = useState(
+		taskToEdit.description
+	);
+	const [priorityField, setPriorityField] = useState('');
+	const [editFormSubmitted, setEditFormSubmitted] = useState(false);
+
+	const handleNameField = (e) => {
+		setNameField(e.target.value);
+	};
+
+	const handleDescriptionField = (e) => {
+		setDescriptionField(e.target.value);
+	};
+
+	const handleRadioChange = (e) => {
+		setPriorityField(e.target.value);
+	};
+
+	const editTask = async () => {
+		const fitsRequirements = [nameField, descriptionField, priorityField].every(
+			(field) => {
+				return field.length > 0;
+			}
+		);
+
+		if (!fitsRequirements) {
+			alert('Must fill all fields');
+		} else {
+			const editedTask = {
+				title: nameField,
+				description: descriptionField,
+				priority: priorityField,
+				id: taskToEdit.id,
+			};
+			try {
+				const returnedTask = await TaskServices.editTask(
+					user.token,
+					editedTask
+				);
+				const newTasks = tasks.map((task) => {
+					if (task.id === taskToEdit.id) {
+						task.title = returnedTask.title;
+						task.description = returnedTask.description;
+						task.priority = returnedTask.priority;
+						return task;
+					} else {
+						return task;
+					}
+				});
+				setTasks(newTasks);
+				setTaskToEdit(null);
+				setFormToOpen(null);
+				setNameField('');
+				setDescriptionField('');
+				setPriorityField('');
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (initialRender.current) {
+			initialRender.current = false;
+		} else {
+			editTask();
+		}
+	}, [editFormSubmitted]);
+
+	return (
+		<form
+			onSubmit={(event) => {
+				event.preventDefault();
+				setEditFormSubmitted(!editFormSubmitted);
+			}}
+		>
+			<h1>Edit the task</h1>
+			<div className="taskNameContainer">
+				<label htmlFor="taskNameInput">Name</label>
+				<input
+					type="text"
+					id="taskNameInput"
+					value={nameField}
+					placeholder="Type your task's title"
+					onChange={handleNameField}
+					autoComplete="off"
+				/>
+			</div>
+			<div className="taskDescriptionContainer">
+				<label htmlFor="taskDescriptionInput">Description</label>
+				<textarea
+					type="textarea"
+					id="taskDescriptionInput"
+					value={descriptionField}
+					placeholder="Type your task's description"
+					onChange={handleDescriptionField}
+				/>
+			</div>
+			<div className="priority">
+				<p>Set the priority</p>
+				<label htmlFor="high">high</label>
+				<input
+					type="radio"
+					name="priority"
+					value="high"
+					onChange={handleRadioChange}
+				/>
+				<label htmlFor="medium">medium</label>
+				<input
+					type="radio"
+					name="priority"
+					value="medium"
+					onChange={handleRadioChange}
+				/>
+				<label htmlFor="low">low</label>
+				<input
+					type="radio"
+					name="priority"
+					value="low"
+					onChange={handleRadioChange}
+				/>
+			</div>
+			<div className="buttonsContainer">
+				<button className="submitButton" type="submit">
+					Submit
+				</button>
+				<button
+					className="cancelButton"
+					type="button"
+					onClick={() => {
+						setTaskToEdit(null);
+						setFormToOpen(null);
+						setNameField('');
+						setDescriptionField('');
+						setPriorityField('');
+					}}
+				>
+					Cancel
+				</button>
+			</div>
+		</form>
+	);
+};
+
+export default EditTaskForm;
