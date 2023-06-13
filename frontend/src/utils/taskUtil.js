8df6b14f6cea2a4d92e3a74c5be31b99b807fcab
me3za <medkrix1@gmail.com>
2023-06-13 20:50:18 +0100

    added fallback logic for sorting tasks when priority is equal

--- frontend/src/utils/taskUtil.js
+++ frontend/src/utils/taskUtil.js
@@ -20,8 +20,20 @@ export const calcTotal = (tasks) => {
 	);
 };
 
+const strcmp = (s1, s2) => {
+	for (let i = 0; i < s1.length && i < s2.length; i++) {
+		if (s1.charCodeAt(i) !== s2.charCodeAt(i)) {
+			return s1.charCodeAt(i) - s2.charCodeAt(i);
+		}
+	}
+
+	return s1.length - s2.length;
+};
+
 export const sortedTasks = (tasks) => {
-	return tasks.sort(
-		(a, b) => priorityValue[b.priority] - priorityValue[a.priority]
-	);
+	return tasks.sort((a, b) => {
+		const byPriority = priorityValue[b.priority] - priorityValue[a.priority];
+		const byTitle = strcmp(a.title, b.title);
+		return byPriority || byTitle;
+	});
 };
